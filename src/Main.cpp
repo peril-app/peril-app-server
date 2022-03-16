b87551c34cfade1ddb9de8834528b42f21fea48c
katznboyz1 <harrison.lindgren@gmail.com>
2022-03-15 20:06:12 -0400

    transferring workstations

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <string>
 
-#include "./oatpp/build/oatpp/src/oatapp/network/Server.hpp"
+#include "oatpp/src/oatpp/web/server/api/ApiController.hpp"
+#include "oatpp/src/oatpp/core/macro/codegen.hpp"
 
-void run() {
+#include OATAPP_CODEGEN_BEGIN(ApiController)
+
+class MainAPIController : public oatpp::web::server::api::ApiController {
+
+    MainAPIController(OATPP_COMPONENT(std::shared_ptr<ObjectMapper>, objectMapper) : oatpp::web::server::api::ApiController(objectMapper)) {}
+
+    ENDPOINT("GET", "/", root) {
+
+        return createResponse(Status::CODE_200, "return homepage information");
+    }
+
+    ENDPOINT("GET", "/auth_action/{actionID}", login, PATH(Int64, actionID)) {
+
+        return createResponse(Status::CODE_200, String.toString(actionID->getValue()));
+    }
 }
+
+#include OATAPP_CODEGEN_END(ApiController)
