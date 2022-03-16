10aa894107474e2f7a5ff210dbc3c1853a8ca869
katznboyz1 <harrison.lindgren@gmail.com>
2022-03-16 19:53:32 -0400

    the server responds now

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#define CROW_MAIN
+
 #include <crow_all.h>
 #include <iostream>
 #include <string>
@@ -5,6 +7,8 @@
 using namespace std;
 
 // the "man in the middle" for requests being sent to and from the server
+// current just here so that I dont forget to add this later
+// does nothing
 struct PerilAppMiddleman {
 
     std::string logMsg;
@@ -16,13 +20,33 @@ struct PerilAppMiddleman {
     struct context {};
 
     // before handling the request - usually stuff like logging
-    void before_handle(crow::request& req, crow::response& res, context& ctx) {cout << logMsg << endl;}
+    void before_handle(crow::request& req, crow::response& res, context& ctx) {cout << "[peril-app-server] " << logMsg << endl;}
 
     // for now, do nothing
     void after_handle(crow::request& req, crow::response& res, context& ctx) {;}
 };
 
+/* 
+    standard route response should be:
+    {
+        "debugMsg":std::string
+    }
+*/
 int main() {
+
+    crow::App<PerilAppMiddleman> app;
+
+    app.get_middleware<PerilAppMiddleman>().setMessage("STARTING PERIL APP SERVER");
+
+    CROW_ROUTE(app, "/")
+    ([] () {
+
+        return "{debugMsg:\"homepage test\"}";
+    });
+
+    app.loglevel(crow::LogLevel::Debug);
+
+    app.port(7348).multithreaded().run();
 
     return 0;
 }
