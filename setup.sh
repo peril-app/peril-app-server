57aa0c0293ab8956d2132ad2c1f902a6d5f0d4c8
katznboyz1 <harrison.lindgren@gmail.com>
2022-03-17 13:51:29 -0400

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,14 @@ echo "[WARNING] THIS IS FOR TESTING ONLY!"
 
 apt update -y
 
-apt install cmake wget libsqlite3-dev -y
+apt install cmake wget libsqlite3-dev clang -y
 
 wget https://github.com/CrowCpp/Crow/releases/download/v0.3%2B4/crow-v0.3+4.deb
 
 apt install ./crow-v0.3+4.deb -y
 
-cmake ./src
+cd src
 
-cmake --build ./src
+cmake .
+
+cmake --build .
